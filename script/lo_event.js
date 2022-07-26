46c88faa1d2c78efd3e4e20b4e358eed1d9ce7d2
logroid <yoheiowa+github@gmail.com>
2022-07-26 17:16:42 +0900

    update

--- script/lo_event.js
+++ script/lo_event.js
@@ -1,5 +1,5 @@
 window.lastOriginEvent = {
-  event_name: "怒りの狼牙",
-  event_start: 1655218800000,
-  event_end: 1658880000000
+  event_name: "光届かぬ聖域",
+  event_start: 1658847600000,
+  event_end: 1662508800000
 }
