47e6ab95f4aa673ef63c406fb4745ae1670ff6bc
logroid <yoheiowa+github@gmail.com>
2022-09-21 13:39:17 +0900

    update

--- script/lo_event.js
+++ script/lo_event.js
@@ -1,5 +1,5 @@
 window.lastOriginEvent = {
-  event_name: "光届かぬ聖域",
-  event_start: 1658847600000,
-  event_end: 1662508800000
+  event_name: "蓮花と陰に咲く薔薇",
+  event_start: 1662476400000,
+  event_end: 1664928000000
 }
