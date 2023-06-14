38c62d6e5ca63b92eb7951a23dfd96b5c458b73e
logroid <yoheiowa+github@gmail.com>
2023-06-14 20:00:06 +0900

    update

--- script/lo_event.js
+++ script/lo_event.js
@@ -1,5 +1,5 @@
 window.lastOriginEvent = {
-  event_name: "オルカデート抽選会",
-  event_start: 1684422000000,
-  event_end: 1686700800000
+  event_name: "蓮花と陰に咲く薔薇",
+  event_start: 1686668400000,
+  event_end: 1689120000000
 }
