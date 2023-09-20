59cc14c829ec85ee0b28324a222f06a7a8b857f8
logroid <yoheiowa+github@gmail.com>
2023-09-20 16:26:35 +0900

    update

--- script/lo_event.js
+++ script/lo_event.js
@@ -1,5 +1,5 @@
 window.lastOriginEvent = {
-  event_name: "誰かが望んだ願い",
-  event_start: 1692716400000,
-  event_end: 1693958400000
+  event_name: "あなたの楔にまごころを",
+  event_start: 1695135600000,
+  event_end: 1698192000000
 }
